ad93a03eb3e3cb3559a259ab3bf27ab35f618ca8
bobur <usmonkulov5@gmail.com>
2023-03-12 16:02:17 +0500

    7.misol Mahsus belgilar \t \n

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-first_name = "James"
-last_name = "Bond"
-print(f"Salom, mening ismim {last_name}, {first_name} {last_name}!")
+
+print("Hello World!")
+print("Hello \tWorld!")
+print("Hello \nWorld!")
