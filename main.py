83c2e10b32e53928da0a49a9d67bc5bbc128be36
bobur <usmonkulov5@gmail.com>
2023-03-12 16:07:49 +0500

    8.misol String metodlar upper() metodi hamma harflarni katta harfga almashtiradi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
-print("Hello World!")
-print("Hello \tWorld!")
-print("Hello \nWorld!")
+first_name = "james"
+last_name = "bond"
+first_name_last_name = f"{first_name} {last_name}"
+print(first_name_last_name.upper())
