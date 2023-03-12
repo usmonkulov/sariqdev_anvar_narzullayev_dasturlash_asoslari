f9fc7c82d1bdb6a31b89e2659647f62f6b4d95be
bobur <usmonkulov5@gmail.com>
2023-03-12 16:11:05 +0500

    9.misol String metodlar lower() metodi hamma harflarni kichik harfga almashtiradi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-first_name = "james"
-last_name = "bond"
+first_name = "JAMES"
+last_name = "BOND"
 first_name_last_name = f"{first_name} {last_name}"
-print(first_name_last_name.upper())
+print(first_name_last_name.lower())
