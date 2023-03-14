7d2f8461bb64b34bcf77c2dd7d87ff43c4510c58
bobur <usmonkulov5@gmail.com>
2023-03-14 20:14:10 +0500

    18. misol TUPLE o'zgarmas ro'yxatni o'zgartirib bo'lmaydi TypeError: 'tuple' object does not support item assignment

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-cars = ['bmw', 'mercedes benz', 'volvo', 'general motors', 'tesla', 'audi']
-my_cars = cars[:]
-my_cars.remove('bmw')
-print(my_cars)
-print(cars)
+toys = ('bus', 'car', 'bear', 'dino', 'snake', 'lizard')
+toys[0] = "teddy"
+print(toys)
