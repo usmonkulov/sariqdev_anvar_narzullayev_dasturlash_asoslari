45e924c665c1637120bcc9586ee86c142a14fb97
bobur <usmonkulov5@gmail.com>
2023-03-14 19:15:40 +0500

    14. misol

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 narhlar = [12000, 22500, 23456, 9800, 5600, 9934, 32874]
-print(sum(narhlar))
+arzon = min(narhlar)
+qiymat = max(narhlar)
+jami = sum(narhlar)
+print("Eng arzon narh ", arzon, ". Eng qimmati ", qiymat, ". Jami: ", jami)
