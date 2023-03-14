081927928a165f7b5e8bee0614e3f7362e44e83d
bobur <usmonkulov5@gmail.com>
2023-03-14 19:19:08 +0500

    15. misol ro'yxatni kesishni qaraymiz ars[0:3]

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-narhlar = [12000, 22500, 23456, 9800, 5600, 9934, 32874]
-arzon = min(narhlar)
-qiymat = max(narhlar)
-jami = sum(narhlar)
-print("Eng arzon narh ", arzon, ". Eng qimmati ", qiymat, ". Jami: ", jami)
+cars = ['bmw', 'mercedes benz', 'volvo', 'general motors', 'tesla', 'audi']
+print(cars[0:3])
