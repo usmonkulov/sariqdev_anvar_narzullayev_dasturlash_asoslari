9554e29307f2f4ef7ef94c59cf595cb8308e3769
bobur <usmonkulov5@gmail.com>
2023-03-15 14:51:18 +0500

    1. misol. tarmoqlanish if agar degani else aks holda degani

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-dostlar = []
-print("5 ta eng yaqin do'stingiz kim?")
-for n in range(5):
-    dostlar.append(input(f"{n+1}-do'stingizning ismini kiriting: "))
-print(dostlar)
+avtolar = ['audi', 'bmw', 'volvo', 'kia', 'hyundai']
+
+for avto in avtolar:
+    if avto == 'bmw':
+        print(avto.upper())
+    else:
+        print(avto.title())
