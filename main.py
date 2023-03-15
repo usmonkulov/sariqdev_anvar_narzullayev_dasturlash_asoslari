31f741b0350b72f26c99f4f08d0486902d92867f
bobur <usmonkulov5@gmail.com>
2023-03-15 19:44:33 +0500

    2. misol.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-avtolar = ['audi', 'bmw', 'volvo', 'kia', 'hyundai']
-
-for avto in avtolar:
-    if avto == 'bmw':
-        print(avto.upper())
-    else:
-        print(avto.title())
+ism = input('Ismingiz nima?\n>>>')
+if ism.lower() != 'ali':
+    print(f"Uzr, {ism.title()} biz Alini kutayapmiz.")
+else:
+    print("Salom Ali")
