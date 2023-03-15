15c4ba11a08786ff7432f82ec43e21fe4363c2e3
bobur <usmonkulov5@gmail.com>
2023-03-15 22:23:34 +0500

    6. misol.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-login = input("Yangi login tanlang:")
-if len(login) <= 5:
-    print("Login 5 harfdan ko'proq bo'lishi shart!")
+yil = int(input("Tug'ilgan yilingizni kiriting:"))
+if 2023-yil < 18:
+    print(f"Yoshingiz {2023-yil}da ekan.")
+    print("Kirish mumkin emas!")
+else:
+    print("Xush kelibsiz!")
