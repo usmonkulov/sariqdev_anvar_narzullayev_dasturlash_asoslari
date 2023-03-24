afb8df17e55c29ee6f79a88386d269346117f005
bobur <usmonkulov5@gmail.com>
2023-03-24 21:18:52 +0500

    9. misol.

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 narh = 15000
 choy = True
 salat = False
+non = True
+kompot = True
+assorti = False
 
-if choy and salat:
-    narh = narh + 10000
-elif choy or salat:
+if choy:
+    print("Mijoz choy oldi.")
+    narh = narh + 3000
+
+if non:
+    print("Mijoz non oldi.")
+    narh = narh + 2000
+
+if salat:
+    print("Mijoz salat oldi.")
     narh = narh + 5000
+
+if kompot:
+    print("Mijoz kompot oldi.")
+    narh = narh + 5000
+
+if assorti:
+    print("Mijoz assorti oldi.")
+    narh = narh + 15000
 
 print(f"Jami {narh} so'm")
