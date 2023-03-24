c178691a7acd965a8b5703ff9dae83f463b77447
bobur <usmonkulov5@gmail.com>
2023-03-24 19:38:05 +0500

    4. misol.

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ if yosh <= 4:
     print("Sizga kirish bepul.")
 elif yosh <= 12:
     print("Sizga kirish 5000 so'm")
+elif yosh <= 18:
+    print("Sizga kirish 8000 so'm")
 else:
     print("Sizga kirish 10000 so'm")
